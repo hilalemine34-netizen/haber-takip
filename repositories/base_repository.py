f9f4ac14a8f7a90826b63229164a2fd34582a8c3
Hilal İlhan <hilalemine34@gmail.com>
2026-06-19 16:15:07 +0300

    Railway DB connection

--- repositories/base_repository.py
+++ repositories/base_repository.py
@@ -1,18 +1,23 @@
+import os
 import mysql.connector
+
 
 class BaseRepository:
 
     def get_connection(self):
         return mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="K334z91h36.",
-            database="news_tracking_db",
+            host=os.getenv("MYSQLHOST"),
+            user=os.getenv("MYSQLUSER"),
+            password=os.getenv("MYSQLPASSWORD"),
+            database=os.getenv("MYSQLDATABASE"),
+            port=int(os.getenv("MYSQLPORT", 3306)),
             autocommit=True
         )
 
     def get_cursor(self, dictionary=False):
         conn = self.get_connection()
-        conn.ping(reconnect=True)  #  otomatik reconnect
-        cursor = conn.cursor(dictionary=dictionary, buffered=True)
+        cursor = conn.cursor(
+            dictionary=dictionary,
+            buffered=True
+        )
         return cursor, conn
