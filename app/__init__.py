846e91451d9f83028a58edfc4ee0c4e900f8902c
Hilal İlhan <hilalemine34@gmail.com>
2026-06-19 17:28:07 +0300

    Fix Render timeout

--- app/__init__.py
+++ app/__init__.py
@@ -29,11 +29,6 @@ def create_app() -> Flask:
 
     app.register_blueprint(news_bp)
     init_web_routes(app)
-    API_KEY = "d9656d853c9a4f7083c43a82b644784c"
-
-    try:
-        NewsFetcher(API_KEY).run()
-    except Exception as e:
-        print("FETCH ERROR:", e)
+    
 
     return app
